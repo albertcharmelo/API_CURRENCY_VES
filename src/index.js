28f8ff73440cdcd38fb293269ae6896897db7cf4
Albert Charmelo <albertcharmelocontacto@gmail.com>
2023-03-05 02:21:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const CurrencyRoutes = require("./routes/CurrencyRoutes");
-
-require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
